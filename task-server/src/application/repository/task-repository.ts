75fe084bf05ba9fdabae97b6499ae71362a541a7
iamrosada <luisrosada2018@gmail.com>
2023-10-12 22:35:54 +0300

    feat: created the task repository, inside this i added all function for our application

---
+++ task-server/src/application/repository/task-repository.ts
@@ -0,0 +1,8 @@
+import { TaskEntity } from "../../domain/task";
+
+export interface TaskRepository{
+  create(task: Omit<TaskEntity, "uuid"| "createdAt"| "updatedAt">):Promise<TaskEntity>
+  delete(notesId:string):Promise<void>
+  update(noteId: string, task: Omit<TaskEntity, "uuid"| "createdAt"| "updatedAt">):Promise<TaskEntity| null>
+  list():Promise<TaskEntity[]>
+}
