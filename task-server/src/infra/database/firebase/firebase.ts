5086676d80cd4f86d40c95a23d48584a0b37a413
iamrosada <luisrosada2018@gmail.com>
2023-10-13 16:25:48 +0300

    feat: updated the database confi

--- task-server/src/infra/database/firebase/firebase.ts
+++ task-server/src/infra/database/firebase/firebase.ts
@@ -1,18 +1,21 @@
 
-import { initializeApp } from "firebase/app";
-import { getFirestore }  from 'firebase-admin/firestore'
-
+import { initializeApp } from 'firebase/app';
+import { initializeFirestore, collection, addDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCf1W2-53tmdkVoDqURn-BSWbU2JTSxRDU",
-  authDomain: "ivipcoin-project.firebaseapp.com",
-  projectId: "ivipcoin-project",
-  storageBucket: "ivipcoin-project.appspot.com",
-  messagingSenderId: "91545958901",
-  appId: "1:91545958901:web:11f3008797bbff9dfb08bc",
-  measurementId: "G-YDS7RZLNJ0"
+  apiKey: "AIzaSyAGhKqvo4S4UYWSFK6VWVeRHSt6pyIVQk4",
+  authDomain: "ivipcoin-project-a6e58.firebaseapp.com",
+  projectId: "ivipcoin-project-a6e58",
+  storageBucket: "ivipcoin-project-a6e58.appspot.com",
+  messagingSenderId: "171220625587",
+  appId: "1:171220625587:web:b380f280eb9d9eb97828c6"
 };
 
 // Initialize Firebase
-initializeApp(firebaseConfig);
-export const db = getFirestore()
+//  const app = firebase.initializeApp(firebaseConfig);
+//  const db = getFirestore()
+const app = initializeApp(firebaseConfig);
+
+const db = initializeFirestore(app, { ignoreUndefinedProperties: true })
+export { app, db };
+
