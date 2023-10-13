0f026c60a83d09fb26156865725468681d1b941d
iamrosada <luisrosada2018@gmail.com>
2023-10-13 14:22:23 +0300

    feat: updated the FirebaseTaskRepository and added the firebase

--- task-server/src/infra/repository/firebase-task-repository.ts
+++ task-server/src/infra/repository/firebase-task-repository.ts
@@ -1,20 +1,61 @@
 import { TaskRepository } from "../../application/repository/task-repository";
 import { TaskEntity } from "../../domain/task";
 
-
+import {
+  collection,
+  addDoc,
+  doc,
+  updateDoc,
+  deleteDoc,
+  getDocs,
+} from 'firebase/firestore';
+import { db } from "../database/firebase/firebase";
 
 export class FirebaseTaskRepository implements TaskRepository {
-  create(task: TaskEntity): Promise<TaskEntity> {
-    throw new Error("Method not implemented.");
-  }
-  delete(notesId: string): Promise<void> {
-    throw new Error("Method not implemented.");
-  }
-  update(noteId: string, task: Omit<TaskEntity, "uuid" | "createdAt" | "updatedAt">): Promise<TaskEntity> {
-    throw new Error("Method not implemented.");
-  }
-  list(): Promise<TaskEntity[]> {
-    throw new Error("Method not implemented.");
+
+  async create(task: TaskEntity): Promise<TaskEntity> {
+    try {
+      const taskCollection = collection(db, 'tasks');
+      const docRef = await addDoc(taskCollection, task);
+      return { ...task, uuid: docRef.id };
+    } catch (error) {
+      throw new Error('Erro ao criar a tarefa: ' + error);
+    }
   }
 
+  async delete(noteId: string): Promise<void> {
+    try {
+      const taskRef = doc(db, 'tasks', noteId);
+      await deleteDoc(taskRef);
+    } catch (error) {
+      throw new Error('Erro ao excluir a tarefa: ' + error);
+    }
+  }
+
+  async update(noteId: string, task: Omit<TaskEntity, "uuid" | "createdAt" | "updatedAt">): Promise<TaskEntity> {
+    try {
+      const taskRef = doc(db, 'tasks', noteId);
+      await updateDoc(taskRef, task);
+      return { ...task, uuid: noteId };
+    } catch (error) {
+      throw new Error('Erro ao atualizar a tarefa: ' + error);
+    }
+  }
+
+  async list(): Promise<TaskEntity[]> {
+    try {
+      const taskCollection = collection(db, 'tasks');
+      const querySnapshot = await getDocs(taskCollection);
+      const tasks: TaskEntity[] = [];
+
+      querySnapshot.forEach((doc) => {
+        const taskData = doc.data() as TaskEntity;
+        tasks.push({ ...taskData, uuid: doc.id });
+      });
+
+      return tasks;
+    } catch (error) {
+      throw new Error('Erro ao listar as tarefas: ' + error);
+    }
+  }
 }
