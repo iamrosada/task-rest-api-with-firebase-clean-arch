14f1982d346c0c349f2e2f06a2690c0fd0461bb8
iamrosada <luisrosada2018@gmail.com>
2023-10-13 16:21:12 +0300

    feat: updated list use case to receive two parameter

--- task-server/src/application/usecase/list-task.ts
+++ task-server/src/application/usecase/list-task.ts
@@ -4,8 +4,8 @@ import { TaskRepository } from "../repository/task-repository";
 export class ListTaskUseCase {
   constructor(private readonly taskRepository: TaskRepository) { }
 
-  public listTask = async () => {
-    const task = await this.taskRepository.list();
+  public listTask = async (startAt:number, itemsPerPage:number) => {
+    const task = await this.taskRepository.list(startAt, itemsPerPage);
     return task;
   };
 
