37ee62e49ee2d5bc7415526dbac3f8dfeaab6154
iamrosada <luisrosada2018@gmail.com>
2023-10-16 21:50:24 +0300

    feat: updated component to list all itens

--- task-front/src/components/TaskList/TaskList.tsx
+++ task-front/src/components/TaskList/TaskList.tsx
@@ -3,15 +3,15 @@ import { Box } from '@mui/material';
 
 import type { Task } from '../../App';
 import { TaskItem } from './TaskItem/TaskItem';
+// eslint-disable-next-line import/no-cycle
 import { TaskPanel } from '../Panel/Panel';
 
 interface TaskListProps {
-  editTaskId: Task['id'] | null;
+  editTaskId: Task['uuid'] | null;
   taskList: Task[];
-  onDeleteTask: (id: Task['id']) => void;
-  onCheckTask: (id: Task['id']) => void;
-  onEdit: (id: Task['id']) => void;
-  onChangeTask: ({ title, description }: Omit<Task, 'id' | 'checked'>) => void;
+  onDeleteTask: (uuid: Task['uuid']) => void;
+  onEdit: (uuid: Task['uuid']) => void;
+  onChangeTask: ({ title, description }: Omit<Task, 'uuid' | 'checked'>) => void;
 }
 
 export const TaskList: React.FC<TaskListProps> = ({
@@ -19,19 +19,17 @@ export const TaskList: React.FC<TaskListProps> = ({
   editTaskId,
   onChangeTask,
   onDeleteTask,
-  onCheckTask,
   onEdit
 }) => (
   <Box>
     {taskList.map((task) => {
-      if (task.id === editTaskId)
-        return <TaskPanel mode='edit' onChangeTask={onChangeTask} editTask={task} />;
+      if (task.uuid === editTaskId)
+        return <TaskPanel key={task.uuid} mode='edit' onChangeTask={onChangeTask} editTask={task} />;
       return (
         <TaskItem
-          key={task.id}
+          key={task.uuid}
           task={task}
           onDeleteTask={onDeleteTask}
-          onCheckTask={onCheckTask}
           onEdit={onEdit}
         />
       );
