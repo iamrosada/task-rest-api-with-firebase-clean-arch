1c200a29fe5e910beb837cd66e7e3c2931c4c8ee
iamrosada <luisrosada2018@gmail.com>
2023-10-16 21:11:45 +0300

    feat: add update ,delete and list function

--- task-front/src/shared/context/response-context.tsx
+++ task-front/src/shared/context/response-context.tsx
@@ -1,85 +1,114 @@
-import React, { createContext, useContext, useState } from 'react';
-import { useClient } from '../api';
+import React, { createContext, useCallback, useContext, useMemo, useState } from 'react';
+import {client} from '../api';
 
 export type Task = {
+  [x: string]: any;
   uuid: string;
   title: string;
   description: string;
-  checked: boolean;
 };
+interface ApiResponse {
+  tasks: Task[];
+}
 
-const TaskContext = createContext({});
+interface TaskContextType {
+  createTaskFn: (title: string, description: string) => Promise<void>;
+  deleteTaskFn: (uuid: string) => Promise<void>; 
+  updateTaskFn: (uuid: string, title: string, description: string) => Promise<Task>;
+  listTasksFn: () => Promise<Task[]>;
+  loginFn: (name: string, password: string) => Promise<string>;
+  logoutFn: () => void;
+  taskItems: Task[];
+}
+
+const TaskContext = createContext<TaskContextType>({
+  logoutFn: () => { },
+  createTaskFn: () => Promise.resolve() ,
+  deleteTaskFn: () => Promise.resolve(),
+  updateTaskFn: () => Promise.resolve() as any,
+  listTasksFn: () => Promise.resolve() as any,
+  loginFn: () => Promise.resolve() as any, 
+  taskItems: []
+});
 
 export const useTaskContext = () => useContext(TaskContext);
 
-export const TaskProvider = ({ children }:any) => {
-  const client = useClient(); 
-  const [taskItens, setTaskItens] = useState<Omit<Task,"uuid"|"checked">>()
+export const TaskProvider = ({ children }: any) => {
+  const [error, setError] = useState<string | any>('');
+  const [taskItems, setTaskItems] = useState<Task[]>([]);
 
-
-  const createTaskFn = async (title:string, description:string) => {
+  const createTaskFn = useCallback(async (title: string, description: string): Promise<void> => {
     try {
-      const response = await client.createTaskFn(title, description);
-       setTaskItens(response.data)
-    } catch (error) {
-      console.error('Error creating task:', error);
+      const response = await client.post<Task>('/tasks', { title, description });
+      console.log(response.data.task,"taskItems")
+
+      setTaskItems((prevTaskItems) => [...prevTaskItems, response.data.task]);
+
+    } catch (error: any) {
+      setError(`Error creating task: ${error.message}`);
       throw error;
     }
-  };
+  }, []);
 
-  const deleteTaskFn = async (uuid:string) => {
+  const deleteTaskFn = useCallback(async (uuid: string): Promise<void> => {
     try {
-      const response = await client.deleteTaskFn(uuid);
-      return response;
-    } catch (error) {
-      console.error('Error deleting task:', error);
+      await client.delete<void>(`tasks/${uuid}`);
+      setTaskItems((prevTaskItems) => prevTaskItems.filter((task) => task.uuid !== uuid));
+    } catch (error: any) {
+      setError(`Error deleting task: ${error.message}`);
       throw error;
     }
-  };
+  }, []);
 
-  const updateTaskFn = async (uuid:string, title:string, description:string) => {
+  const updateTaskFn = useCallback(async (uuid: string, title: string, description: string): Promise<Task> => {
     try {
-      const response = await client.updateTaskFn(uuid, title, description);
-      return response;
-    } catch (error) {
-      console.error('Error updating task:', error);
+      const response = await client.patch<Task>(`/tasks/${uuid}`, { uuid, title, description });
+      setTaskItems((prevTaskItems) =>
+        prevTaskItems.map((task) => (task.uuid === uuid ? { ...task, title, description } : task))
+      );
+      return response.data.task;
+    } catch (error: any) {
+      setError(`Error updating task: ${error.message}`);
       throw error;
     }
-  };
+  }, []);
 
-  const listTasksFn = async () => {
+  const listTasksFn = useCallback(async (): Promise<Task[]> => {
     try {
-      const response = await client.listTasksFn();
-      return response;
-    } catch (error) {
-      console.error('Error listing tasks:', error);
+      const response = await client.get<ApiResponse>('/tasks');
+      setTaskItems([...taskItems, ...(response.data?.tasks || [])]);
+      return response.data?.tasks;
+    } catch (error: any) {
+      setError(`Error listing tasks: ${error.message}`);
       throw error;
     }
-  };
+  }, []);
 
-  const loginFn = async (name:string, password:string) => {
+  const loginFn = useCallback(async (name: string, password: string): Promise<string> => {
     try {
-      const token = await client.loginFn(name, password);
+      const response = await client.post<string>('/tasks/login', { name, password });
+      const token = response.data;
+      localStorage.setItem('x-access-token', token);
       return token;
-    } catch (error) {
-      console.error('Login error:', error);
+    } catch (error: any) {
+      setError(`Login error: ${error.message}`);
       throw error;
     }
-  };
-
+  }, []);
   const logoutFn = () => {
-    client.logoutFn();
+    localStorage.removeItem('x-access-token');
   };
 
-  // eslint-disable-next-line react/jsx-no-constructed-context-values
-  const contextValue = {
+  const contextValue = useMemo(() => ({
     createTaskFn,
     deleteTaskFn,
     updateTaskFn,
     listTasksFn,
     loginFn,
     logoutFn,
-  };
+    taskItems,
+    error,
+  }), [taskItems, error]);
 
   return (
     <TaskContext.Provider value={contextValue}>
