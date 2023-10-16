3bcada05bd884e5403e90e2907d0408bfb694a8f
iamrosada <luisrosada2018@gmail.com>
2023-10-17 02:16:53 +0300

    fix: error on reload

--- task-front/src/App.tsx
+++ task-front/src/App.tsx
@@ -1,6 +1,8 @@
-import React, {  useEffect } from 'react';
-import { Box } from '@mui/material';
-// eslint-disable-next-line import/no-cycle
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable import/no-cycle */
+import React, { useEffect, useMemo, useState } from 'react';
+import { Box, Button, CircularProgress, Pagination, Theme, createStyles } from '@mui/material';
+import { NavigateBefore, NavigateNext } from '@mui/icons-material';
 import { Header, TaskPanel, TaskList } from './components';
 import { useTaskContext } from './shared/context/response-context';
 
@@ -12,28 +14,69 @@ export type Task = {
 
 export const App = () => {
   const [editTaskId, setEditTaskId] = React.useState<string | null>(null);
-  const { createTaskFn, deleteTaskFn, updateTaskFn, listTasksFn,taskItems } = useTaskContext();
+  const { createTaskFn, deleteTaskFn, updateTaskFn, listTasksFn, taskItems } = useTaskContext();
   const [taskList, setTaskList] = React.useState<Task[]>(taskItems);
 
-  const onAddTask = async (title:string, description:string) => {
+  const [sizeReal, setSizeReal] = React.useState<number>(1);
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [currentPage, setCurrentPage] = React.useState(1);
+  const itemsPerPage: number = 2;
+  const [isLoading, setIsLoading] = useState(false);
+
+  const loadTasks = async (page: number, itemsPerPage: number) => {
+    setIsLoading(true);
+    try {
+      const startIndex = (page - 1) * itemsPerPage;
+      const endIndex = startIndex + itemsPerPage;
+      const response = await listTasksFn(startIndex, endIndex);
+
+      setTaskList(response.tasks);
+      setSizeReal(response.size);
+      setIsLoading(false); 
+    } catch (error) {
+      console.error('Error loading tasks:', error);
+      setIsLoading(false); 
+    }
+  };
+
+
+  const handleNextPage = () => {
+   
+    if (currentPage * itemsPerPage < sizeReal) {
+      const nextPage = currentPage + 1;
+      setCurrentPage(nextPage);
+      loadTasks(nextPage, itemsPerPage);
+    }
+  };
+
+  const handlePreviousPage = () => {
+    if (currentPage > 1) {
+      const previousPage = currentPage - 1;
+      setCurrentPage(previousPage);
+      loadTasks(previousPage, itemsPerPage);
+    }
+  };
+
+  useEffect(() => {
+    loadTasks(currentPage, itemsPerPage);
+  }, [itemsPerPage]);
+
+  const onAddTask = async (title: string, description: string) => {
     try {
       const response = await createTaskFn(title, description);
       const newTask = response as any;
 
       setTaskList((prevTaskList) => [...prevTaskList, newTask]);
-
     } catch (error) {
       console.error('Error creating a task:', error);
     }
   };
 
-
-
   const onDeleteTask = async (uuid: Task['uuid']) => {
     try {
       await deleteTaskFn(uuid);
-      const response = await listTasksFn();
-      setTaskList(response);
+      loadTasks(currentPage, itemsPerPage);
     } catch (error) {
       console.error('Error deleting a task:', error);
     }
@@ -59,46 +102,59 @@ export const App = () => {
   };
 
   useEffect(() => {
-    
     const updateTaskList = async () => {
-      try {
-        const response = await listTasksFn();
-        setTaskList(response); 
-      } catch (error) {
-        console.error('Error updating task list:', error);
-      }
+      loadTasks(currentPage, itemsPerPage);
     };
+    updateTaskList();
+  }, [currentPage, itemsPerPage]);
 
-    updateTaskList(); 
 
-  }, [taskList, listTasksFn]); 
-  
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await listTasksFn();
-        console.log(response,"dentro do effect")
-        setTaskList([...taskList, ...(response || [])]);
-      } catch (error) {
-        console.error('Error fetching task list:', error);
-      }
-    };
-  
-    fetchData(); 
-  }, []);
+  const getCurrentTasks = (tasks: any) => {
+    const indexOfLastTask = currentPage * itemsPerPage;
+    const indexOfFirstTask = indexOfLastTask - itemsPerPage;
+    return tasks.slice(indexOfFirstTask, indexOfLastTask);
+  };
+
   return (
     <Box marginTop={5} height='100%' display='flex' justifyContent='center' alignContent='center'>
-      <Box display='flex' flexDirection='column' width='500px'>
-        <Header taskCount={taskList.length} />
-        <TaskPanel mode='add' onAddTask={onAddTask} />
+    <Box display='flex' flexDirection='column' width='500px'>
+      <Header taskCount={taskList.length} />
+      <TaskPanel mode='add' onAddTask={onAddTask} />
+      
+      {isLoading ? ( 
+      <Box display='flex' justifyContent='center' alignItems='center' marginTop={2}>
+        <CircularProgress/>
+        </Box>
+      ) : (
         <TaskList
           editTaskId={editTaskId}
-          taskList={taskList}
+          taskList={getCurrentTasks(taskList)}
           onDeleteTask={onDeleteTask}
           onEdit={onEdit}
           onChangeTask={onChangeTask}
         />
+      )}
+
+      <Box display='flex' justifyContent='center' alignItems='center' marginTop={2}>
+        <Button
+          variant='outlined'
+          disabled={currentPage === 1}
+          onClick={handlePreviousPage}
+          startIcon={<NavigateBefore />}
+        >
+          Previous
+        </Button>
+        <span>Page {currentPage}</span>
+        <Button
+          variant='outlined'
+          disabled={currentPage * itemsPerPage >= sizeReal}
+          onClick={handleNextPage}
+          endIcon={<NavigateNext />}
+        >
+          Next
+        </Button>
       </Box>
     </Box>
+  </Box>
   );
 };
