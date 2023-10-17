ecb33982a1b2d7fb2767329243bee712570df1cd
iamrosada <luisrosada2018@gmail.com>
2023-10-17 12:29:05 +0300

    feat: added context api

--- task-front/src/index.tsx
+++ task-front/src/index.tsx
@@ -4,6 +4,11 @@ import ReactDOM from 'react-dom/client';
 import { App } from './App';
 
 import './index.css';
+import { TaskProvider } from './shared/context/response-context';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(<App />);
+root.render(
+  <TaskProvider>
+    <App />
+  </TaskProvider>
+);
