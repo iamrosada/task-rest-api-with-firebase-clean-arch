06166cc2dc9e036bf3461328f1cf203623d2f17f
iamrosada <luisrosada2018@gmail.com>
2023-10-17 12:33:04 +0300

    feat: created called api using interceptors

---
+++ task-front/src/shared/api.ts
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+const API_BASE_URL = 'http://localhost:8080';
+
+
+export const client = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  withCredentials: true,
+});
+
+client.interceptors.request.use(
+  (config) => {
+    const updatedConfig = { ...config } as any; 
+    const token = localStorage.getItem('x-access-token');
+    if (token) {
+      updatedConfig.headers = {
+        ...updatedConfig.headers,
+        'x-access-token': token,
+      };
+    }
+    return updatedConfig;
+  },
+  (error) => Promise.reject(error)
+);
+
