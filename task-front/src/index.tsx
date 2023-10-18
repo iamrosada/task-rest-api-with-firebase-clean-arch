c7eb30dd0cdd83d4e11032738d08527fa8f33aab
iamrosada <luisrosada2018@gmail.com>
2023-10-18 12:12:48 +0300

    feat: added auth provider context to project

--- task-front/src/index.tsx
+++ task-front/src/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { App } from './App';
+// import { App } from './App';
 
 import './index.css';
 import { TaskProvider } from './shared/context/response-context';
+import { AuthProvider } from './shared/context/auth-context';
+import AppRoutes from './routes';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <TaskProvider>
-    <App />
-  </TaskProvider>
+  <AuthProvider>
+    <TaskProvider>
+      <AppRoutes/>
+    </TaskProvider>
+  </AuthProvider>
 );
