4ec4c3b186242455138a20c2c79a60d1b9ef3c01
iamrosada <luisrosada2018@gmail.com>
2023-10-18 12:08:24 +0300

    feat: added news libs

--- task-server/src/infra/http/routes.ts
+++ task-server/src/infra/http/routes.ts
@@ -1,22 +1,43 @@
 import express from "express";
 import { app } from "../../server-task";
-import { handlerCreateTask, handlerDeleteTask, handlerGetTasks, handlerUpdateTask } from "../core/handler/handler";
-
-
+import {
+  handlerCreateTask,
+  handlerDeleteTask,
+  handlerGetTasks,
+  handlerUpdateTask,
+} from "../core/handler/handler";
+import {
+  handlerLogInWithEmailAndPassword,
+  handlerRegisterWithEmailAndPassword,
+  handlerSignInWithGoogle,
+  handlerSendPasswordReset,
+  handlerLogout,
+} from "../core/handler/handler-auth";
+import { Middleware } from "../core/middlewares/ensureAuthenticated";
+const middlewareInstance = new Middleware();
 
 export default function runServer() {
   const router = express.Router();
 
-
-  app.post('/tasks', handlerCreateTask);
-  
-  app.get('/tasks', handlerGetTasks);
-  
-  app.patch('/tasks/:uuid',handlerUpdateTask);
-  
-  app.delete('/tasks/:uuid', handlerDeleteTask);
+  // app.use(middlewareInstance.decodeToken)
+  app.post("/tasks", handlerCreateTask);
 
 
-  app.use('/', router);
+  // app.use(middlewareInstance.decodeToken)
+  app.get("/tasks", handlerGetTasks);
+
+  // app.use(middlewareInstance.decodeToken)
+  app.patch("/tasks/:uuid", handlerUpdateTask);
+
+  // app.use(middlewareInstance.decodeToken)
+  app.delete("/tasks/:uuid", handlerDeleteTask);
+
+  // Authentication-related routes
+  app.post("/auth/login", handlerLogInWithEmailAndPassword);
+  app.post("/auth/register", handlerRegisterWithEmailAndPassword);
+  app.get("/auth/signInWithGoogle", handlerSignInWithGoogle);
+  app.post("/auth/sendPasswordReset", handlerSendPasswordReset);
+  app.get("/auth/logout", handlerLogout);
+
+  app.use("/", router);
 }
-
