6de4ae832c61e70eec92c808ea39a8eecd9677e5
iamrosada <luisrosada2018@gmail.com>
2023-10-18 12:11:05 +0300

    feat: created user auth repository

--- task-server/src/application/repository/auth-repository.ts
+++ task-server/src/application/repository/auth-repository.ts
@@ -1,12 +1,13 @@
 import { AuthEntity } from "../../domain/authentication"
+import { OutPutUserAuth } from "../../infra/dto/dto"
 
 
 export interface AuthRepository{
   signInWithGoogle():Promise<void>
-  logInWithEmailAndPassword(auth:Omit<AuthEntity,'uuid' |"name" | "createdAt" |
-  "updatedAt">):Promise<void>
-  registerWithEmailAndPassword(auth:Omit<AuthEntity,'uuid' |"name"| "createdAt" |
-  "updatedAt">):Promise<void>
+  logInWithEmailAndPassword(auth:Omit<AuthEntity,'uuid'|"createdAt" |
+  "updatedAt">):Promise<OutPutUserAuth>
+  registerWithEmailAndPassword(auth:Omit<AuthEntity, "createdAt" |
+  "updatedAt">):Promise<OutPutUserAuth>
   sendPasswordReset(email:string):Promise<void>
   logout():Promise<void>
 }
